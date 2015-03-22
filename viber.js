e15503cc552af9380c5a31ed7d8bce1892169fd7
Zach Pomerantz <zmp@umich.edu>
2015-03-21 21:58:20 -0700

    (feat) Variable vibratory refractory length

--- viber.js
+++ viber.js
@@ -1,8 +1,22 @@
 var INTERVAL = Math.floor(1000 / 60)
-var VIB_LENGTH = 250
+var VIB_LENGTH = 500 
+
+// UI
+
+
+// Business Logix
 var pollInterval = 0
+
 navigator.vibrate =
   navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate
+
+window.onload = function() {
+  var crank = document.querySelector('#refractory')
+
+  crank.value = +VIB_LENGTH
+
+  crank.addEventListener('change', function() { VIB_LENGTH = +crank.value })
+}
 
 // Signal vibration presence
 navigator.vibrate([500, 500, 500, 500])
