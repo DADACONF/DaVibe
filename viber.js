5e9942539e8709fa6995ff7a918f997f8d056af9
Zach Pomerantz <zmp@umich.edu>
2015-03-21 22:08:10 -0700

    (feat) Poll continuously

--- viber.js
+++ viber.js
@@ -2,14 +2,6 @@ var INTERVAL = Math.floor(1000 / 60)
 var VIB_LENGTH = 500 
 
 // UI
-
-
-// Business Logix
-var pollInterval = 0
-
-navigator.vibrate =
-  navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate
-
 window.onload = function() {
   var crank = document.querySelector('#refractory')
 
@@ -18,25 +10,24 @@ window.onload = function() {
   crank.addEventListener('change', function() { VIB_LENGTH = +crank.value })
 }
 
+// Business Logix
+navigator.vibrate =
+  navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate
+
 // Signal vibration presence
 navigator.vibrate([500, 500, 500, 500])
 
-setTimeout(function() {
-  var pollInterval = setInterval(poll, INTERVAL)
-}, 2000)
+setTimeout(poll, 2000)
 
 function poll() {
-  var pitch = pitcher().pitch
+  var pitch
 
-  if (!pitch || pitch > 650) {
-    // Resume polling
-    if (!pollInterval) { pollInterval = setInterval(poll, INTERVAL) }
+  while (true) {
+    pitch = pitcher().pitch
+    if (!pitch || pitch > 650) { continue }
 
-    return
+    navigator.vibrate(VIB_LENGTH)
+    setTimeout(poll, VIB_LENGTH)
+    break
   }
-
-  clearInterval(pollInterval)
-
-  navigator.vibrate(VIB_LENGTH)
-  setTimeout(poll, VIB_LENGTH)
 }
