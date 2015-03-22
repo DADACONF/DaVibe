eaabbca81100cf9dc39e927b308e61dd2e7e4830
Zach Pomerantz <zmp@umich.edu>
2015-03-21 21:46:06 -0700

    (feat) Vibrate for 250 ms

--- viber.js
+++ viber.js
@@ -1,18 +1,28 @@
 var INTERVAL = Math.floor(1000 / 60)
+var VIB_LENGTH = 250
+var pollInterval = 0
 navigator.vibrate =
   navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate
 
-// Test vibration 
+// Signal vibration presence
 navigator.vibrate([500, 500, 500, 500])
 
 setTimeout(function() {
-  setInterval(poll, INTERVAL)
+  var pollInterval = setInterval(poll, INTERVAL)
 }, 2000)
 
 function poll() {
   var pitch = pitcher().pitch
 
-  if (!pitch || pitch > 650) { return }
+  if (!pitch || pitch > 650) {
+    // Resume polling
+    if (!pollInterval) { pollInterval = setInterval(poll, INTERVAL) }
 
-  navigator.vibrate(INTERVAL)
+    return
+  }
+
+  clearInterval(pollInterval)
+
+  navigator.vibrate(VIB_LENGTH)
+  setTimeout(poll, VIB_LENGTH)
 }
